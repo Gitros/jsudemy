9c62ad9ea7f0154c4fab1879e75ddf7e385ae666
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-28 18:46:07 +0200

    ex3

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,15 @@
-let x = 100;
-let y = 50;
+const promo = '20%';
 
-if (x > y) {
-    console.log('x > y');
-} else if (x === y) {
-    console.log('x = y');
-} else if (x < y) {
-    console.log('x < y');
+switch (promo) {
+    case '10%':
+        console.log('promocja na 10%');
+        break;
+    case '20%': console.log('promocja na 20%');
+        break;
+    case '30%': console.log('promocja na 30%');
+        break;
+
+    default:
+        console.log('20 %');
 }
+
