1d8f8617951094a4c5af45819aa47372bc7737eb
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-29 19:36:00 +0100

    ex4

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,13 @@
-let x = 20;
+let numbers = [5, 8, 10, 23, 48, 60];
 
-do {
-    x -= 3;
-    console.log(x);
-} while (x > 0);
+for (let divine of numbers) {
+    console.log(divine / 5);
+}
+
+for (let number of numbers) {
+    if (number % 2 === 0) {
+        console.log(`%cLiczba ${number} jest parzysta`);
+    } else {
+        console.log(`%cLiczba ${number} jest nieparzysta`), 'background-color: red';
+    }
+}
