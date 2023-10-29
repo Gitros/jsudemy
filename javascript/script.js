685471dcacf822dc84ccc99e0288bcb9d68ab841
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-29 18:51:12 +0100

    ex1

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,5 @@
-const numbers = [1, 2, 3, 4, 5];
+const miasta = ['Rzeszów', 'Kraków', 'Warszawa', 'Katowice', 'Lublin'];
 
-// for (let i = 0; i < numbers.length; i++) {
-//     console.log(numbers[i] * 2);
-// }
-
-for (const number of numbers) {
-    console.log(number * 2);
+for (let i = 0; i < miasta.length; i++) {
+    console.log(`To miasto nazywa się ${miasta[i]}`)
 }
