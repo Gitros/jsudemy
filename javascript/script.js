6cd84afae4acfc0d785fb95da122bf450d5f9f6d
Gitros <31899790+Gitros@users.noreply.github.com>
2023-10-30 12:13:32 +0100

    arrays

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,8 @@
-const numbers = [1, 2, 3, 4, 5, 6];
+const numbers = [1, 2, 3, 4, 5];
 
-// const btn1 = document.querySelector('button:nth-of-type(1)')
-// const btn2 = document.querySelector('button:nth-of-type(2)')
-// const btn3 = document.querySelector('button:nth-of-type(3)')
+numbers.unshift(-1, 0);
 
-const btns = document.querySelectorAll('button')
 
-function btnMsg(e) {
-    console.log(`kliknięto ${e.target.textContent} ! 🐱`);
-}
+console.log(numbers);
 
-// btn1.addEventListener('click', btnMsg)
-btns.forEach(btn => btn.addEventListener('click', btnMsg))
+numbers.shift();
