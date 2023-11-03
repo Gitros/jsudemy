4a61cd64d11be9c4571a137a82fd0e40a1d95d49
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-03 14:57:55 +0100

    methods

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,13 @@
 const numbers = [0, 23, 48, 175, 2, 34, 11];
+
+// function number(x) {
+//     // return x % 2 === 0
+//     return x > 50
+// }
+
+// console.log(numbers.filter(number));
+
+numbers.forEach(number => console.log(number + 5))
+
+console.log(numbers.includes(0));
+console.log(numbers.indexOf(23));
