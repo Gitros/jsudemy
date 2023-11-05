26b131766462a4fc1fbf5ef6b2a0a6102bbdb191
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-05 13:57:02 +0100

    function

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,29 @@
-const color = ['niebieski'];
+// declaration of a function
 
-color.push('czerwony');
-color.unshift('fioletowy');
-
-for (let i = 0; i < color.length; i++) {
-    console.log(`Mój ulubiony kolor to: ${color[i].charAt(0).toUpperCase() + color[i].slice(1)}`);
+function test() {
+    console.log('cześć');
 }
+
+test();
+
+// functional expression
+
+const helloWorld = function () {
+    console.log('cześć! 123');
+}
+
+helloWorld();
+
+// function with arguments
+
+function add(x, y) {
+    return x + y;
+}
+
+add(5, 7);
+
+const name = function (name, age) {
+    console.log(`Cześć mam na imie ${name} i mam ${age} lat. `);
+}
+
+name('jakub', 25);
