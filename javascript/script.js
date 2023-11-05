33fa87d1d3e73884e1379d1ac7580fe0e5eb75af
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-05 14:01:56 +0100

    anonymus function

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+const heading = document.querySelector('h1');
+
+function test() {
+    console.log('kliknieto mnie');
+}
+
+heading.addEventListener('click', test)
+
+
+
+
+
+// heading.addEventListener('click', function () {
+//     console.log('kliknieto mnie');
+// })
