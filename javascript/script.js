a4486b6d4d3a4627d0e2e9f59fbaaebafce00173
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-05 14:18:24 +0100

    arrow pt2

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+//Cześć imie jak sie masz?
+
+const hello = (name = 'drogi użytkowniku') => {
+    console.log(`Cześć ${name}, jak się masz`);
+}
+
+hello()
+
+const add = (x, y) => {
+    console.log(x + y);
+}
+
+add()
