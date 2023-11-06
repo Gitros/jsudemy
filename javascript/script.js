26c0b17256b526fe3ec6ad89c4cdbe110a2f4b72
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-06 14:34:09 +0100

    query selector ex

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const heading = document.querySelector('h1');
+console.log(heading);
+
+const test = document.querySelectorAll('p');
+console.log(test);
+
+const test2 = document.querySelector('.test');
+console.log(test2);
+
+const test3 = test2.querySelector('#test');
+
+console.log(test3);
