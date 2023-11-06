f2e3a337990858a0d09a584754d9edaa1d7bf92b
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-06 14:29:56 +0100

    pt2

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,30 @@
 // //getElementById
-// const test = document.getElementById('item');
+const test = document.getElementById('item');
 
 // //getElementsByTagName
-// const test2 = document.getElementsByClassName('ul');
+ const test2 = document.getElementsByClassName('ul');
 
 // //getElementsByClassName
-// const test3 = document.getElementsByClassName('test');
+ const test3 = document.getElementsByClassName('test');
 
+//querySelector(ES6)
+const test4 = document.querySelector('li');
+
+const ulList = document.querySelector('ul');
+console.log(ulList);
+
+const liItem = ulList.querySelector('li');
+console.log(liItem);
+
+//querySelectorAll (ES6)
+
+const liItems = document.querySelectorAll('li.test');
+console.log(liItem);
+
+
+
+//Żywe kolekcje, czyli qSA vs getElementsByTag / ClassName
+
+const newLi = document.createElement('li');
+ulList.appendChild(newLi).textContent = '😎';
+console.log(liItems.length);
