b847b06b146821aeaf9ac7039d9dffc3237146ba
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-06 15:44:05 +0100

    clear

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-const div = document.querySelector('div');
-const p = document.createElement('p');
-const headingTwo = document.createElement('h2');
-
-// div.appendChild(p);
-// div.appendChild(headingTwo);
-
-div.append(p, headingTwo, 'cześć');
