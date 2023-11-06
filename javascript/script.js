1b60bba166489d9af5f5524ccba92873c57e4d03
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-06 16:23:40 +0100

    textcontent, innertext outerhtml innerhtml

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('button');
+
+// btn.outerHTML = '123';
+
+// btn.innerHTML = '<li>abcd</li>';
+//document.body.innerHTML = '';
+
+console.log(btn.innerText);
+console.log(btn.textContent);
+
+// btn.textContent = '<li>abcd</li>';
+// btn.innerText = '<li>abcd</li>'
