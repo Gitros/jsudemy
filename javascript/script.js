3d9cce9c419b85111c9fe6daaffe45427eb4a5c4
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-06 14:11:19 +0100

    pobieranie elementów

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+//getElementById
+const test = document.getElementById('item');
+console.log(test);
+//getElementsByTagName
+const test2 = document.getElementsByClassName('ul');
+console.log(test2);
+
+//getElementsByClassName
+
+const test3 = document.getElementsByClassName('test');
+console.log(test3);
