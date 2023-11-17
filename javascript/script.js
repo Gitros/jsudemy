3e36c1b2cddd7bed317f38c9f0d98c1329f13c32
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-17 16:53:00 +0100

    tools

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,6 @@ let addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let ulList; // lista zadań, tagi UL
 let newTodo; //nowo dodany LI nowe zadanie
 
-
 const main = () => {
     prepareDOMElemnts();
     prepareDomEvents();
@@ -25,12 +24,35 @@ const addNewTodo = () => {
     if (todoInput.value !== '') {
         newTodo = document.createElement('li');
         newTodo.textContent = todoInput.value;
+        createToolsArea();
+
         ulList.append(newTodo);
+
         todoInput.value = '';
         errorInfo.textContent = ''
     } else {
         errorInfo.textContent = 'wpisz treść zadania!'
     }
+}
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    newTodo.append(toolsPanel)
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class = "fas fa-check"></i>';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times">';
+
+    toolsPanel.append(completeBtn, editBtn, deleteBtn);
 }
 
 
