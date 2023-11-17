6b1c86e38f29e79436a72e0afd1a4f14d4adf4cf
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-17 16:34:54 +0100

    adding new tasks

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,8 @@ let todoInput; // miejsce gdzie użytkownik wpisuje treść zadania
 let errorInfo; // info o braku zadań / konieczności wpisania tekstu
 let addBtn; // przycisk ADD - dodaje nowe elementy do listy
 let ulList; // lista zadań, tagi UL
+let newTodo; //nowo dodany LI nowe zadanie
+
 
 const main = () => {
     prepareDOMElemnts();
@@ -9,14 +11,28 @@ const main = () => {
 }
 
 const prepareDOMElemnts = () => {
-    todoInput = document.querySelector('.todoInput');
+    todoInput = document.querySelector('.todo-input');
     errorInfo = document.querySelector('.error-info');
     addBtn = document.querySelector('.btn-add');
     ulList = document.querySelector('.todolist ul');
 }
 
 const prepareDomEvents = () => {
-    //nadajemy nasłuchiwanie
+    addBtn.addEventListener('click', addNewTodo);
 }
+
+const addNewTodo = () => {
+    if (todoInput.value !== '') {
+        newTodo = document.createElement('li');
+        newTodo.textContent = todoInput.value;
+        ulList.append(newTodo);
+        todoInput.value = '';
+        errorInfo.textContent = ''
+    } else {
+        errorInfo.textContent = 'wpisz treść zadania!'
+    }
+}
+
+
 
 document.addEventListener('DOMContentLoaded', main);
