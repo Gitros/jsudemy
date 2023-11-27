17158a432b5e4b64b95efe6d32914b3238b0780e
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-27 23:09:03 +0100

    klasy

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,76 @@
-const movie = {
-    title: 'Avengers',
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
-function showMovie(price, cinema) {
-    console.log(`Film ${this.title}, cena: ${price}, kino ${cinema},`);
+Person.prototype.sayHi = function () {
+    console.log(`Cześć, jestem ${this.name}!`);
 }
 
+Person.prototype.showAge = function () {
+    console.log(`Mam ${this.age} lat/a`);
+}
 
-showMovie(30, 'SuperKino');
+const person1 = new Person('Majek', 23);
 
-// funkcja.call(obiekt, argumenty)
-showMovie.call(movie, 35, 'SuperKino2');
+person1.sayHi();
+person1.showAge();
 
-// funkcja.apply(obiekt, tablica)
-const movieData = [35, 'superKino2']
+// ====================
 
-showMovie.apply(movie, movieData);
+class Person2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi() {
+        console.log(`Cześć, jestem ${this.name}!`);
+    }
+
+    showAge() {
+        console.log(`Mam ${this.age} lat/a`);
+    }
+}
+
+const person2 = new Person2('ania', 23);
+
+console.log(person2);
+
+function Gra(name, version) {
+    this.name = name;
+    this.version = version;
+}
+
+Gra.prototype.newGame = function () {
+    console.log(`Witaj w nowej grze ${this.name}!`);
+}
+
+Gra.prototype.newVersion = function () {
+    console.log(`Nowy wymiar gry w wersji ${this.version}`);
+}
+
+const gra = new Gra('Skyrim', 5);
+
+gra.newGame();
+gra.newVersion();
+
+// ===============
+
+class Gra2 {
+    constructor(name, version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    newGame2() {
+        console.log(`Witaj w nowej grze ${this.name}!`);
+    }
+    newVersion2() {
+        console.log(`Nowy wymiar gry w wersji ${this.version}`);
+    }
+}
+
+const gra2 = new Gra2('Wiedzmin', 3);
+gra2.newGame2()
+gra2.newVersion2()
