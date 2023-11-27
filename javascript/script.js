2a90923b1621ed223835f809ee4987b59dbc06d3
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-27 11:00:47 +0100

    Constructor

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,21 @@
-function User
+// konstruktor
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.hello = function () {
+        console.log(`Cześć ${this.name}`);
+    }
+}
+
+const user = {
+    name: 'Klaudia',
+    age: 23,
+}
+console.log(user.name);
+
+const newUser = new User('Klaudia', 23);
+const newUser2 = new User('Lily', 34);
+console.log(newUser);
+
+newUser2.hello()
