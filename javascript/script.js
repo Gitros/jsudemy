22b2b375130863bc26b6bd00fe6d140d1735561e
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-27 10:20:40 +0100

    objects

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// const myObject = {
+//     key:value,
+//     key:value,
+//     key:value,
+// }
+
+const newUser = {
+    name: 'lisa',
+    age: 23,
+    car: {
+        brand: 'Audi',
+        model: 'A3',
+        color: 'czarny',
+    }
+}
