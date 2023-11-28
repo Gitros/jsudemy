10f6e17d0395e6d5f50abc3c9d2929de8115197f
Gitros <31899790+Gitros@users.noreply.github.com>
2023-11-28 11:33:34 +0100

    async await

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+const URL = 'https://dog.ceo/api/breeds/image/random';
+
+fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+async function test() {
+    try {
+        const res = await fetch(URL);
+        const data = await res.json();
+
+        console.log(data);
+    } catch {
+        console.error('blad');
+    }
+}
+
+test();
